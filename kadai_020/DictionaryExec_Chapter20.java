6f9ff6068822f508302f7b940c5fa95625c29ac1
DARMAGRANDPRIX <163404531+DARMAGRANDPRIX@users.noreply.github.com>
2024-04-16 09:51:30 +0900

    Update DictionaryExec_Chapter20.java

--- kadai_020/DictionaryExec_Chapter20.java
+++ kadai_020/DictionaryExec_Chapter20.java
@@ -3,9 +3,10 @@ package kadai_020;
 public class DictionaryExec_Chapter20 {
 
 	public static void main(String[] args) {
+		Dictionary_Chapter20 myDictionary = new Dictionary_Chapter20();
 		String [] wordsArray = {"apple","banana","grape","orange"};
 		for (int i = 0; i < wordsArray.length; i++) {
-		new Dictionary_Chapter20(wordsArray[i]);
+			myDictionary.serch(wordsArray[i]);
 		}
      }
 }
